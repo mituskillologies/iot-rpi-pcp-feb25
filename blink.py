28d7d987635053ab3dc06cbfab83d3d5b888614f
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-02-15 18:53:26 +0530

    Add files via upload

---
+++ blink.py
@@ -0,0 +1,15 @@
+# Blinking the LED
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+LED_PIN = 18
+GPIO.setup(LED_PIN, GPIO.OUT)
+try:
+	while True:
+		GPIO.output(LED_PIN, GPIO.HIGH)
+		time.sleep(1)
+		GPIO.output(LED_PIN, GPIO.LOW)
+		time.sleep(1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	exit(0)
